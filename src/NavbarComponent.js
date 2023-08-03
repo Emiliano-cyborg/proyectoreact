995ed4eec21f3f252a6116f2d25986e9d69611fc
Emiliano <Emiliano.leonel.arias@outlook.com>
2023-08-02 22:56:03 -0300

    Arreglos

--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -6,7 +6,7 @@ const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Mi Ecommerce</Navbar.Brand>
+        <Navbar.Brand href="#home">Don Gato</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
