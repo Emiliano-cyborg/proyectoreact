04ac10ecf04005454a554fb57baaa00c2600f00d
Emiliano <Emiliano.leonel.arias@outlook.com>
2023-10-02 22:55:41 -0300

    Browser

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavbarComponent from './NavbarComponent';
 import ItemListContainer from './ItemListContainer';
 import Productos from './Productos';
+import { Switch, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div>
-      <NavbarComponent />
-      <div style={contentStyle}>
-        <h1>SuperMercado Don Gato</h1>
-        <ItemListContainer greeting="¡Bienvenido a nuestro sitio de ecommerce!" />
-        <div style={productsContainer}>
-          <Productos />
-          
+    <Router>
+      <div>
+        <NavbarComponent />
+        <div style={contentStyle}>
+          <h1>SuperMercado Don Gato</h1>
+          {/* Configuramos las rutas */}
+          <Switch>
+            <Route path="/" exact render={() => <ItemListContainer greeting="¡Bienvenido a nuestro sitio de ecommerce!" />} />
+            <Route path="/productos" component={Productos} />
+            {/* Agrega más rutas según sea necesario */}
+          </Switch>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
 const contentStyle = {
   margin: '20px',
-};
-
-const productsContainer = {
-  display: 'flex',
-  flexWrap: 'wrap',
 };
 
 export default App;
